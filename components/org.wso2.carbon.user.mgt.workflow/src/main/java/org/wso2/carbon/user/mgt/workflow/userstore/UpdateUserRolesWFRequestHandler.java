65fe08809035bf7e0b64b99b02f9218a334ae642
sadilchamishka <sandilchamishka@gmail.com>
2025-08-10 09:14:50 +0530

    Address comments

--- components/org.wso2.carbon.user.mgt.workflow/src/main/java/org/wso2/carbon/user/mgt/workflow/userstore/UpdateUserRolesWFRequestHandler.java
+++ components/org.wso2.carbon.user.mgt.workflow/src/main/java/org/wso2/carbon/user/mgt/workflow/userstore/UpdateUserRolesWFRequestHandler.java
@@ -226,7 +226,7 @@ public class UpdateUserRolesWFRequestHandler extends AbstractWorkflowRequestHand
             try {
                 if (UserStoreWFConstants.ENTITY_TYPE_USER.equals(entities[i].getEntityType()) && workflowService
                         .entityHasPendingWorkflowsOfType(entities[i], UserStoreWFConstants.DELETE_USER_EVENT)) {
-                    throw new WorkflowException("User is pending on a delete workfloww which clocks this operation.");
+                    throw new WorkflowException("User is pending on a delete workflow which blocks this operation.");
                 } else if (UserStoreWFConstants.ENTITY_TYPE_ROLE.equals(entities[i].getEntityType()) && (workflowService
                         .entityHasPendingWorkflowsOfType(entities[i], UserStoreWFConstants.DELETE_ROLE_EVENT) ||
                         workflowService.entityHasPendingWorkflowsOfType(entities[i], UserStoreWFConstants
