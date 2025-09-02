723bb4b8b99c9612f9fb747b8c9a7dfce29cb006
ImalshaD <plid475@gmail.com>
2025-09-02 20:07:19 +0530

    Fix Authorization Bug.

--- components/org.wso2.carbon.user.mgt.workflow/src/main/java/org/wso2/carbon/user/mgt/workflow/UserManagementWorkflowService.java
+++ components/org.wso2.carbon.user.mgt.workflow/src/main/java/org/wso2/carbon/user/mgt/workflow/UserManagementWorkflowService.java
@@ -18,19 +18,46 @@
 
 package org.wso2.carbon.user.mgt.workflow;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.CarbonConstants;
 import org.wso2.carbon.context.CarbonContext;
+import org.wso2.carbon.identity.core.util.IdentityConfigParser;
 import org.wso2.carbon.identity.workflow.mgt.WorkflowManagementService;
 import org.wso2.carbon.identity.workflow.mgt.exception.WorkflowException;
+import org.wso2.carbon.user.api.AuthorizationManager;
+import org.wso2.carbon.user.api.UserStoreException;
 import org.wso2.carbon.user.mgt.workflow.internal.IdentityWorkflowDataHolder;
 
+import java.util.HashMap;
 import java.util.List;
 
+import static org.wso2.carbon.user.mgt.workflow.util.UserStoreWFConstants.ADD_ROLE_EVENT;
+import static org.wso2.carbon.user.mgt.workflow.util.UserStoreWFConstants.ADD_USER_EVENT;
+import static org.wso2.carbon.user.mgt.workflow.util.UserStoreWFConstants.DELETE_ROLE_EVENT;
+import static org.wso2.carbon.user.mgt.workflow.util.UserStoreWFConstants.DELETE_USER_EVENT;
 /**
  * User Management Workflow Service class.
  */
 public class UserManagementWorkflowService {
 
+    private static final Log log = LogFactory.getLog(UserManagementWorkflowService.class);
+
     WorkflowManagementService workflowService = IdentityWorkflowDataHolder.getInstance().getWorkflowService();
+
+    private static final String WORKFLOW_ADMIN_PERMISSION_CONFIG =
+            "AdminServices.UserManagementWorkflowService.WorkflowServicePermission";
+    private static final String DEFAULT_WORKFLOW_ADMIN_PERMISSION =
+            "/permission/admin/manage/identity/workflow/association/view";
+    private static final String USER_VIEW_PERMISSION = "/permission/admin/manage/identity/usermgt/view";
+    private static final String ROLE_VIEW_PERMISSION = "/permission/admin/manage/identity/rolemgt/view";
+    private static final HashMap<String, String> WORKFLOW_OPERATION_PERMISSIONS = new HashMap<String, String>() {{
+        put(ADD_USER_EVENT, USER_VIEW_PERMISSION);
+        put(ADD_ROLE_EVENT, ROLE_VIEW_PERMISSION);
+        put(DELETE_USER_EVENT, USER_VIEW_PERMISSION);
+        put(DELETE_ROLE_EVENT, ROLE_VIEW_PERMISSION);
+    }};
 
     /**
      * Retrieve List of associated Entity-types of the workflow requests.
@@ -47,10 +74,61 @@ public class UserManagementWorkflowService {
             entityIdFilter) throws WorkflowException {
 
         int tenantID = CarbonContext.getThreadLocalCarbonContext().getTenantId();
+        String loggedInName = CarbonContext.getThreadLocalCarbonContext().getUsername();
 
+        if (!isUserAuthorized(tenantID, wfOperationType)) {
+            throw new WorkflowException("Unauthorized access!! User " + loggedInName + " does not have permission to " +
+                    "perform this operation.");
+        }
         List<String> entityNames = workflowService.listEntityNames(wfOperationType, wfStatus, entityType, tenantID,
                 entityIdFilter);
         return entityNames;
 
+    }
+
+    private boolean isUserAuthorized(int tenantId, String wfOperationType) throws WorkflowException {
+
+        String loggedInName = CarbonContext.getThreadLocalCarbonContext().getUsername();
+        String tenantDomain = CarbonContext.getThreadLocalCarbonContext().getTenantDomain();
+
+        AuthorizationManager authzManager = null;
+        try {
+            authzManager = IdentityWorkflowDataHolder.getInstance().getRealmService()
+                    .getTenantUserRealm(tenantId).getAuthorizationManager();
+        } catch (UserStoreException e) {
+            throw new WorkflowException("Error occurred while retrieving AuthorizationManager for tenant "
+                    + tenantDomain, e);
+        }
+
+        boolean isAuthorized = false;
+        try {
+            // Check for the generic workflow admin permission.
+            String permission = (String) IdentityConfigParser.getInstance().getConfiguration()
+                    .get(WORKFLOW_ADMIN_PERMISSION_CONFIG);
+            if (StringUtils.isEmpty(permission)) {
+                permission = DEFAULT_WORKFLOW_ADMIN_PERMISSION;
+            }
+            isAuthorized = authzManager.isUserAuthorized(loggedInName, permission,
+                    CarbonConstants.UI_PERMISSION_ACTION);
+
+            if (log.isDebugEnabled()) {
+                log.debug("User does not have workflow general admin permission. Hence checking for specific " +
+                        "operation permission.");
+            }
+
+            // Check for specific operation permission if not authorized yet.
+            if (!isAuthorized && StringUtils.isNotEmpty(wfOperationType)) {
+                if (WORKFLOW_OPERATION_PERMISSIONS.containsKey(wfOperationType)) {
+                    isAuthorized = authzManager.isUserAuthorized(loggedInName,
+                            WORKFLOW_OPERATION_PERMISSIONS.get(wfOperationType), CarbonConstants.UI_PERMISSION_ACTION);
+                }
+            }
+
+        } catch (UserStoreException e) {
+            throw new WorkflowException("Error occurred while checking access level for " +
+                    "user " + loggedInName + " in tenant " + tenantDomain, e);
+        }
+
+        return isAuthorized;
     }
 }
